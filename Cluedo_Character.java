ae1a4e386546033ab641db7e24ce45565bc8d4f7
KiowaDaly <kiowa.daly@ucdconnect.ie>
2018-01-29 19:19:26 +0000

    Character class
    Basic class object to make the characters

---
+++ Cluedo_Character.java
@@ -0,0 +1,17 @@
+package cluedo;
+
+public class Cluedo_Character {
+	
+		private String characters_name;
+		
+		public Cluedo_Character(String name) {
+			this.characters_name = name;
+		}
+		@Override
+		public String toString() {
+			return characters_name;
+		}
+
+	}
+
+
